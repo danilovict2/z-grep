529fbf678a5a85b8507ad306a4de08c4303389c6
Danilo Sindric <danilosindric@gmail.com>
2025-09-27 14:56:47 -0400

    Update: add support for searching the contents of multiple files

--- src/main.zig
+++ src/main.zig
@@ -1,30 +1,6 @@
 const std = @import("std");
 const matcher = @import("matcher/matcher.zig");
-
-fn matchPattern(input_line: []const u8, pattern: []const u8) !bool {
-    return try matcher.matches(input_line, pattern);
-}
-
-fn matchFile(path: []const u8, pattern: []const u8) !bool {
-    const file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
-
-    const allocator = std.heap.page_allocator;
-    const contents = try file.readToEndAlloc(allocator, 64);
-    defer allocator.free(contents);
-
-    var lines = std.mem.splitScalar(u8, contents, '\n');
-    var ok: bool = false;
-    while (lines.next()) |line| {
-        if (try matcher.matches(line, pattern)) {
-            try std.io.getStdOut().writeAll(line);
-            try std.io.getStdOut().writeAll("\n");
-            ok = true;
-        }
-    }
-
-    return ok;
-}
+const FILE_MAX_LEN: usize = 512;
 
 pub fn main() !void {
     var buffer: [1024]u8 = undefined;
@@ -40,8 +16,8 @@ pub fn main() !void {
     }
 
     const pattern = args[2];
-    if (args.len == 4) {
-        if (try matchFile(args[3], pattern)) {
+    if (args.len > 3) {
+        if (try matchFiles(args[3..], pattern, allocator)) {
             std.debug.print("Match\n", .{});
             std.process.exit(0);
         }
@@ -61,4 +37,50 @@ pub fn main() !void {
 
     std.debug.print("Not a match\n", .{});
     std.process.exit(1);
+}
+
+fn matchFiles(paths: [][:0]u8, pattern: []const u8, allocator: std.mem.Allocator) !bool {
+    var ok: bool = false;
+    for (paths) |path| {
+        if (matchFile(path, pattern, allocator)) |matchingLines| {
+            for (matchingLines) |line| {
+                if (paths.len > 1) {
+                    try std.io.getStdOut().writeAll(path);
+                    try std.io.getStdOut().writeAll(":");
+                }
+
+                try std.io.getStdOut().writeAll(line);
+                try std.io.getStdOut().writeAll("\n");
+            }
+
+            ok = ok or matchingLines.len > 0;
+        } else |err| {
+            return err;
+        }
+    }
+
+    return ok;
+}
+
+fn matchFile(path: []const u8, pattern: []const u8, allocator: std.mem.Allocator) ![][]const u8 {
+    const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    const contents = try file.readToEndAlloc(allocator, FILE_MAX_LEN);
+    defer allocator.free(contents);
+
+    var matchingLines = std.ArrayList([]const u8).init(allocator);
+    var lines = std.mem.splitScalar(u8, contents, '\n');
+    while (lines.next()) |line| {
+        if (try matcher.matches(line, pattern)) {
+            const dup_line = try allocator.dupe(u8, line);
+            try matchingLines.append(dup_line);
+        }
+    }
+
+    return matchingLines.toOwnedSlice();
+}
+
+fn matchPattern(input_line: []const u8, pattern: []const u8) !bool {
+    return matcher.matches(input_line, pattern);
 }
